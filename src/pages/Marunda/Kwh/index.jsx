e2ed69cd83c056f66f3fbcfc63e4c50b93d10779
dian <diancandra112@gmail.com>
2024-02-19 13:55:23 +0700

    idx

--- src/pages/Marunda/Kwh/index.jsx
+++ src/pages/Marunda/Kwh/index.jsx
@@ -975,9 +975,7 @@ export const KwhComponent = () => {
                         "ml-1 leading-normal -mt-[6px] mb-[6px] bottom-2 left-1 text-center"
                       }
                     >
-                      {convertNumberSm(
-                        parseFloat(data.export.minimum_charge_total)
-                      )}
+                      {convertNumberSm(parseFloat(data.export.pemakian_kwh))}
                     </p>
                   </td>
                   <td
@@ -990,7 +988,9 @@ export const KwhComponent = () => {
                         "ml-1 leading-normal -mt-[6px] mb-[6px] bottom-2 left-1 text-center"
                       }
                     >
-                      {convertNumberSm(parseFloat(data.export.pemakian_kwh))}
+                      {convertNumberSm(
+                        parseFloat(data.export.minimum_charge_total)
+                      )}
                     </p>
                   </td>
                   <td
